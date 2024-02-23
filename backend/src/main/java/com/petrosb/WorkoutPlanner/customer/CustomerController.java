a731c8098a2acc84fe440ce2a2d365eb303a51d4
petrosb <petrosbompotis@gmail.com>
2024-02-23 13:13:52 +0200

    define CustomerController

--- backend/src/main/java/com/petrosb/WorkoutPlanner/customer/CustomerController.java
+++ backend/src/main/java/com/petrosb/WorkoutPlanner/customer/CustomerController.java
@@ -1,4 +1,42 @@
 package com.petrosb.WorkoutPlanner.customer;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/customers")
 public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("{customerId}")
+    public Customer getCustomer(@PathVariable("customerId") Long customerId) {
+        return customerService.getCustomer(customerId);
+    }
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest request){
+        customerService.addCustomer(request);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Long customerId){
+        customerService.deleteCustomerById(customerId);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@RequestBody CustomerUpdateRequest updateRequest, @PathVariable("customerId") Long customerId) {
+        customerService.updateCustomer(updateRequest, customerId);
+    }
 }
