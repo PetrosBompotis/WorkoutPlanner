dbf889b468eb56edc09f4dad6e5d76f3c95bbc62
petrosb <petrosbompotis@gmail.com>
2024-03-08 12:13:14 +0200

    1st try to write login in main activity

--- frontend/app/src/main/java/com/example/workoutplanner/MainActivity.java
+++ frontend/app/src/main/java/com/example/workoutplanner/MainActivity.java
@@ -1,18 +1,60 @@
 package com.example.workoutplanner;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SHARED_PREFS_NAME = "MyPrefs";
+    private static final String PREF_TOKEN_KEY = "token";
+
+    RequestQueue requestQueue;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+        sharedPreferences = getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    private boolean isLogged(){
+        return true;
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutMain, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void saveTokenToSharedPreferences(String token) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PREF_TOKEN_KEY, token);
+        editor.apply();
+    }
+
+    public void showToastLong(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void showMessage(String title, String message){
+        new AlertDialog.Builder(this).setTitle(title).setMessage(message).setCancelable(true).show();
     }
 }
